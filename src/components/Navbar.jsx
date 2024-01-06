58a12b67d4a04a7bd14b495b533288afa3b24e2a
Nader Zaman <naderzaman50@gmail.com>
2024-01-06 22:20:14 +0500

    Added react Smooth Scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(false);
   const links = [
     {
       id: 1,
@@ -13,15 +14,15 @@ const Navbar = () => {
       link: "about",
     },
     {
-      id: 1,
+      id: 3,
       link: "portfolio",
     },
     {
-      id: 1,
+      id: 4,
       link: "experience",
     },
     {
-      id: 1,
+      id: 5,
       link: "contact",
     },
   ];
@@ -36,24 +37,30 @@ const Navbar = () => {
             key={id}
             className="px-4 text-gray-500 cursor-pointer capitalize  hover:scale-105 duration-200"
           >
-            {link}
+            <Link to={link} smooth duration={500}>
+              {link}
+            </Link>
           </li>
         ))}
       </ul>
-      <div onClick={()=>setNav(!nav)} className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden ">
-        {nav? <FaTimes size={30}/>:<FaBars size={30}/> }
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden "
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
-      {nav&& <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500 ">
-      {links.map(({ id, link }) => (
-          <li
-            key={id}
-            className="px-4 text-gray-500 cursor-pointer capitalize py-6 text-4xl"
-          >
-            {link}
-          </li>
-        ))}
-      </ul>}
-      
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500 ">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 text-gray-500 cursor-pointer capitalize py-6 text-4xl"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
